6b2f4f6c5c1407546fa04c6c0faed4a893ac7806
Nicolis15 <Nicolistrabajo@gmail.com>
2025-03-15 17:21:48 -0500

    se implemento el apartado toolbox

--- src/Components/Principal/Copy.jsx
+++ src/Components/Principal/Copy.jsx
@@ -1,7 +1,7 @@
 export default function Copy() {
     return (
         <div>
-            <footer class="bg-secundary-blue text-center text-secundary-white p-5 text-[10px] md:text-lg">
+            <footer class="bg-secundary-blue text-center text-secundary-white p-3 text-[10px] md:text-sm">
                 <p>&copy; 2025 Nicolas Lis Cruz. All rights reserved.</p>
             </footer>
         </div>
--- src/Components/Principal/SobreMi.jsx
+++ src/Components/Principal/SobreMi.jsx
@@ -9,7 +9,7 @@ export default function SobreMi() {
 
                 <div class="flex flex-col justify-center items-center pl-5 pr-5 pb-5 md:pl-5  md:pr-15" >
                     <h1 class="font-code-bold text-secundary-white md:text-3xl sm:text-2xl text-start w-full">About me</h1>
-                    <p className="font-code text-secundary-white pt-5 text-start text-xs sm:text-xs md:text-sm">
+                    <p class="font-code text-secundary-white pt-5 text-start text-xs sm:text-xs md:text-sm">
                     Soy un ingeniero de sistemas apasionado por el desarrollo frontend, tanto en el ámbito móvil como web. A lo largo de mi carrera, he desarrollado aplicaciones nativas para Android con Kotlin y Jetpack Compose, así como aplicaciones híbridas con React Native, creando experiencias fluidas y eficientes.<br></br><br></br>
                     En el desarrollo web, trabajo con tecnologías como React, Vite y Tailwind CSS para construir interfaces modernas, optimizadas y atractivas. Además, he tenido la oportunidad de desempeñarme como ingeniero de datos, utilizando herramientas como SQL Server, Python, SSIS y Power BI para transformar datos en información valiosa.<br></br><br></br>
                     Me considero una persona disciplinada y enfocada en la mejora continua, siempre en busca de nuevos retos y aprendizajes. Mi objetivo es seguir creciendo profesionalmente y aportar soluciones innovadoras a los proyectos en los que participo.<br></br>
