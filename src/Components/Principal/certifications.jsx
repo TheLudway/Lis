c2591056ed191873f22325eca8290b2f39175763
Nicolis15 <Nicolistrabajo@gmail.com>
2025-03-22 20:23:24 -0500

    Se implemento el apartado de certificados

--- src/Components/Principal/certifications.jsx
+++ src/Components/Principal/certifications.jsx
@@ -3,54 +3,45 @@ import imgScrum from '../../Assets/imgScrum.jpg'
 export default function Certifications() {
     return (
         <section>
-            <div class="flex flex-col justify-center items-center pl-5 pr-5 " >
+            <div class="flex flex-col justify-center items-center pl-5 pr-5 pt-5" >
                 <h1 class="font-code-bold text-secundary-blue text-2xl md:text-3xl sm:text-2xl text-center w-full">Certifications</h1>
             </div>
 
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-6 place-items-center p-5">
+            <div class="flex flex-wrap justify-center items-center gap-10 pt-10 md:pl-20 md:pr-20">
 
-
-                <div class=" flex drop-shadow-xl place-self-end w-fit rounded-md border-2 border-secundary-lightGreen">
-                    <div class="flex flex-wrap  overflow-hidden shadow-lg bg-white p-4 ">
-                        <img class=" w-[40%] max-w-60 rounded-md" src={imgScrum} alt="Certificado" />
-                        <div class=" flex-1 pl-3 pr-3" >
-                            <h3 class=" text-lg font-code-bold text-gray-800">Scrum master</h3>
-                            <p class="text-xs md:text- font-code text-gray-500 ">Scrum - Institude | 2024</p>
+                <a href="https://www.coursera.org/account/accomplishments/verify/LTWSZ60KR3V6" target="_blank" rel="noopener noreferrer">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-react object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">React Native</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
                         </div>
                     </div>
-                </div>
+                </a>
 
-                <div class=" flex drop-shadow-xl place-self-start w-fit rounded-md border-2 border-secundary-lightGreen">
-                    <div class="flex flex-wrap  overflow-hidden shadow-lg bg-white p-4 ">
-
-                        <div class="flex-1 pl-3 pr-3" >
-                            <h3 class=" text-lg font-code-bold text-gray-800">Scrum master</h3>
-                            <p class="text-xs md:text- font-code text-gray-500 ">Scrum - Institude | 2024</p>
+                <a href="https://www.scrum-institute.org/badges/09984443522989" target="_blank" rel="noopener noreferrer">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-scrum object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Scrum master</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Scrum - Institude | 2024</p>
                         </div>
-                        <img class=" w-[40%] max-w-60 rounded-md self-end" src={imgScrum} alt="Certificado Scrum" />
-
                     </div>
-                </div>
+
+                </a>
+
+                <a href="https://www.coursera.org/account/accomplishments/verify/LTWSZ60KR3V6" target="_blank" rel="noopener noreferrer">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-react object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">React Native</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
+                        </div>
+                    </div>
+                </a>
+
+
 
             </div>
 
-            <div class="flex flex-wrap justify-center items-center gap-10">
 
-                <div class="bg-[url(./Assets/imgScrum.jpg)] bg-cover" >
-                    <div class="absolute inset-0 bg-black/15"></div>
-                    <div>
-                        <h3 class=" text-lg font-code-bold ">Scrum master</h3>
-                        <p class="text-xs md:text- font-code ">Scrum - Institude | 2024</p>
-                    </div>
-
-                </div>
-
-            
-                <div class="bg-[url(./Assets/imgScrum.jpg)]">
-                    <h1>Hola</h1>
-                </div>
-
-            </div>
 
 
 
