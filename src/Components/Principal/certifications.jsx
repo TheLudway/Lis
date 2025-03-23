cc3c2b04b714166c27acc057fd89a895acaa94d3
Nicolis15 <Nicolistrabajo@gmail.com>
2025-03-22 21:04:27 -0500

    se agrego el apartado certifications

--- src/Components/Principal/certifications.jsx
+++ src/Components/Principal/certifications.jsx
@@ -9,16 +9,16 @@ export default function Certifications() {
 
             <div class="flex flex-wrap justify-center items-center gap-10 pt-10 md:pl-20 md:pr-20">
 
-                <a href="https://www.coursera.org/account/accomplishments/verify/LTWSZ60KR3V6" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.credly.com/badges/68d8f3cc-cc8d-417a-a78c-b5891ecedd04/print" target="_blank" rel="noopener noreferrer" class="scroll-animation">
                     <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
-                        <div class="flex flex-col fondo-react object-cover bg-cover justify-center items-center bg-center w-full h-full ">
-                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">React Native</h3>
+                        <div class="flex flex-col fondo-meta-developer object-cover bg-cover justify-center items-center bg-center w-full h-full">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Andorid Meta Developer</h3>
                             <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
                         </div>
                     </div>
                 </a>
 
-                <a href="https://www.scrum-institute.org/badges/09984443522989" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.scrum-institute.org/badges/09984443522989" target="_blank" rel="noopener noreferrer" class="scroll-animation">
                     <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
                         <div class="flex flex-col fondo-scrum object-cover bg-cover justify-center items-center bg-center w-full h-full ">
                             <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Scrum master</h3>
@@ -28,10 +28,55 @@ export default function Certifications() {
 
                 </a>
 
-                <a href="https://www.coursera.org/account/accomplishments/verify/LTWSZ60KR3V6" target="_blank" rel="noopener noreferrer">
+                <a href="https://www.coursera.org/account/accomplishments/verify/LTWSZ60KR3V6" target="_blank" rel="noopener noreferrer" class="scroll-animation">
                     <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
                         <div class="flex flex-col fondo-react object-cover bg-cover justify-center items-center bg-center w-full h-full ">
                             <h3 class=" text-2xl font-code-bold text-secundary-white text-center">React Native</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://www.coursera.org/account/accomplishments/specialization/893GG2R2GDPX" target="_blank" rel="noopener noreferrer" class="scroll-animation">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-JavaScript object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Mobile Development and JavaScript</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://www.coursera.org/account/accomplishments/verify/F4J83XMW3B3S" target="_blank" rel="noopener noreferrer" class="scroll-animation">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-JavaScript object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Advanced Programming in Kotlin</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://www.coursera.org/account/accomplishments/verify/5ZY4U300LCYU" target="_blank" rel="noopener noreferrer" class="scroll-animation">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-JavaScript object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Android App Capstone</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://www.coursera.org/account/accomplishments/verify/552L6TUL7LE9" target="_blank" rel="noopener noreferrer" class="scroll-animation">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-JavaScript object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Principles of UX/UI Design</h3>
+                            <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
+                        </div>
+                    </div>
+                </a>
+
+                <a href="https://www.coursera.org/account/accomplishments/verify/2S9F74BRSNRK" target="_blank" rel="noopener noreferrer" class="scroll-animation">
+                    <div class="flex flex-wrap w-xs h-50 border-2 overflow-hidden border-secundary-blue rounded-2xl ">
+                        <div class="flex flex-col fondo-JavaScript object-cover bg-cover justify-center items-center bg-center w-full h-full ">
+                            <h3 class=" text-2xl font-code-bold text-secundary-white text-center">Version Control</h3>
                             <p class="text-sm font-code text-secundary-white text-center">Coursera - Meta | 2024</p>
                         </div>
                     </div>
