b9108b99cb6d7587b6d87e4abfb8fd68bbc60423
Nicolis15 <Nicolistrabajo@gmail.com>
2025-03-25 21:42:11 -0500

    Se Agrego NavBar

--- src/Components/Principal/NabBar.jsx
+++ src/Components/Principal/NabBar.jsx
@@ -1,7 +1,13 @@
 export default function NavBar(){
     return(
-        <nav>
-            
+        <nav class="w-full fixed z-50 bg-black p-3 bg-transparent-">
+            <ul class="flex text-white justify-center gap-16">
+                <li><a href="#sobre-mi" class="font-code-bold hover:underline">About Me</a></li>
+                <li><a href="#herramientas" class="font-code-bold hover:underline">Tool Box</a></li>
+                <li><a href="#experiencia" clas class="font-code-bold hover:underline">Proyects and Experience</a></li>
+                <li><a href="#certificaciones" class="font-code-bold hover:underline">certifications</a></li>
+                <li><a href="#contacto" class="font-code-bold hover:underline">Contact me</a></li>
+            </ul>
         </nav>
     )
 }
--- src/Screens/Principal.jsx
+++ src/Screens/Principal.jsx
@@ -5,18 +5,20 @@ import Herramientas from "../Components/Principal/Herramientas"
 import Exeperiencia from "../Components/Principal/Experiencia"
 import Contacto from "../Components/Principal/Contacto"
 import Certifications from "../Components/Principal/certifications"
+import NavBar from "../Components/Principal/NabBar"
 
-export default function Principal(){
-    return(
+export default function Principal() {
+    return (
         <>
+            <NavBar/>
             <div class="bg-primary ">
-                <Portada/>
-                <SobreMi/>
-                <Herramientas/>
-                <Exeperiencia/>
-                <Certifications/>
-                <Contacto/>
-                <Copy/>
+                <div id="portada"><Portada /></div>
+                <div id="sobre-mi"><SobreMi /></div>
+                <div id="herramientas"><Herramientas /></div>
+                <div id="experiencia"><Exeperiencia /></div>
+                <div id="certificaciones"><Certifications /></div>
+                <div id="contacto"><Contacto /></div>
+                <Copy />
             </div>
         </>
     )
