f53eecf0d0d48769a454429bb1a861fa7bddeb60
Nicolis15 <Nicolistrabajo@gmail.com>
2025-03-28 17:24:59 -0500

    detalles, nada importante

--- src/Components/Principal/NabBar.jsx
+++ src/Components/Principal/NabBar.jsx
@@ -10,19 +10,19 @@ export default function NavBar() {
     return (
       <nav className="fixed top-0 left-0 w-full bg-black p-3 z-50 shadow-md">
         <div className="flex items-center justify-between">
-            
+
           <button onClick={() => setNavegador(!navegador)} className="md:hidden">
             <img src={Menu} alt="Menu Logo" className="w-8" />
           </button>
   
-          <ul className={`absolute top-full left-0 w-full bg-black p-3 transition-all ${
+          <ul className={`absolute top-full left-0 w-full bg-black p-2 transition-all ${
             navegador ? "flex flex-col" : "hidden"
           } md:flex md:flex-row md:static md:w-auto md:bg-transparent gap-6 text-white`}>
-            <li><a href="#sobre-mi" className="font-code-bold hover:underline">About Me</a></li>
-            <li><a href="#herramientas" className="font-code-bold hover:underline">Tool Box</a></li>
-            <li><a href="#experiencia" className="font-code-bold hover:underline">Projects and Experience</a></li>
-            <li><a href="#certificaciones" className="font-code-bold hover:underline">Certifications</a></li>
-            <li><a href="#contacto" className="font-code-bold hover:underline">Contact me</a></li>
+            <li><a href="#sobre-mi" className="sm:text-sm font-code-bold hover:underline">About Me</a></li>
+            <li><a href="#herramientas" className="sm:text-sm  font-code-bold hover:underline">Tool Box</a></li>
+            <li><a href="#experiencia" className="sm:text-sm font-code-bold hover:underline">Projects and Experience</a></li>
+            <li><a href="#certificaciones" className="sm:text-sm font-code-bold hover:underline">Certifications</a></li>
+            <li><a href="#contacto" className="sm:text-sm font-code-bold hover:underline">Contact me</a></li>
           </ul>
   
           {/* Redes Sociales */}
--- src/Components/Principal/Portada.jsx
+++ src/Components/Principal/Portada.jsx
@@ -3,7 +3,7 @@ import programingLogo from '../../Assets/logoProgramacion.svg'
 
 export default function Portada() {
     return (
-        <section class=" g-linear-to-r from-primary to-primary-darkBlue pt-14 md:pt-18" >
+        <section class=" g-linear-to-r from-primary to-primary-darkBlue pt-14 md:pt-16" >
             <div class="fondo">
                 <div class="flex flex-col items-center justify-center pt-20 pb-30">
                     <img src={programingLogo} alt='logo programacion' class="w-30 md:w-40 fill-secundary-white"/>
